e508bd1153d66035046e399d9f11d8ca2547e5de
Jaffy <jaffy@vananaz.com>
2023-05-10 13:38:14 +0800

    Update elements.ts

--- client/src/components/atoms/Select/elements.ts
+++ client/src/components/atoms/Select/elements.ts
@@ -3,6 +3,10 @@ import { Select } from '@mui/material';
 
 const StyledSelect = styled(Select)`
   min-width: 120px;
+
+  .MuiSelect-select {
+    padding: 8.5px 14px;
+  }
 `;
 
 export default StyledSelect;
